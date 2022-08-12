36e714908f06b8ba222b122bb3182e77cea99f39
Ayush Sharma <ayush2162020@gmail.com>
2022-08-12 22:43:20 +0530

    minor corrections resolved

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -9,15 +9,10 @@ import { setInvestorDeals } from "../../redux/createDealSlice";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
+  window.scroll(0, 0);
 
-<<<<<<< HEAD
   const investorDeals = useSelector((state) => state.investorDeals);
-=======
-  // investordeals data to be mapped
-  const investorDeals = useSelector(
-    (state) => state.investorDeal.investorDeals
-  );
->>>>>>> e1884cca9cafb97dca11e1e6f1b9dbb130c12585
+
   const [isLoading, setIsLoading] = useState(true);
 
   const getInvestorDeals = async () => {
@@ -47,19 +42,11 @@ const Dashboard = () => {
           <div className="R_Container">
             {isLoading ? (
               <h3>
-<<<<<<< HEAD
                 Fetching <HourglassSplit /> Investor Deals
               </h3>
             ) : (
               investorDeals.investorDeals.map((data) => (
                 <DisplayCard key={data.id} data={data} />
-=======
-                Fetching <HourglassSplit /> Deals
-              </h3>
-            ) : (
-              investorDeals.map((data) => (
-                <DealCard key={data.id} data={data} />
->>>>>>> e1884cca9cafb97dca11e1e6f1b9dbb130c12585
               ))
             )}
           </div>
